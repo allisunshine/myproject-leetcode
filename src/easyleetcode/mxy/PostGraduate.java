c6dc3688cfb7560237ebc4aa05ac9641517fc428
buer <970946598@qq.com>
2023-03-14 19:03:06 +0800

    求数组对角线之和

--- src/easyleetcode/mxy/PostGraduate.java
+++ src/easyleetcode/mxy/PostGraduate.java
@@ -10,21 +10,26 @@ public class PostGraduate {
 
     /**
      * 从键盘输入一个 3x3 的矩阵，求该矩阵主对角线上的元素之和
+     *
      * @return
      */
     public static int arraysum() {
         int sum = 0, a[][] = new int[3][3];
         System.out.println("请输入3*3的矩阵");
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                Scanner scanner = new Scanner(System.in);
                 a[i][j] = Integer.parseInt(scanner.next());
             }
         }
         for (int i = 0; i < 3; i++) {
-            sum+=a[i][i];
+            sum += a[i][i];
         }
         return sum;
+    }
+
+    public static int maxInThree() {
+        return 0;
     }
 
 }
